4630c65e27561ca859b38e9e0f359ac9a7cecb99
Ralph Holzmann <rholzmann@twitter.com>
2014-05-21 00:01:56 -0500

    Lots of cleanup. Upgraded all deps.

--- app.js
+++ app.js
@@ -1,91 +1,79 @@
-// Dependencies
-var express     = require( "express" ),
-    formidable  = require('formidable'),
-    fs          = require('fs'),
-    join        = require('path').join,
-    imageDir   = join( __dirname, "public"),
-    moment      = require("moment"),
-    app         = express.createServer(),
-    npid        = require("npid");
+var express     = require("express");
+var fs          = require("fs");
+var join        = require('path').join;
+var moment      = require("moment");
+var serveStatic = require('serve-static');
+var multiparty  = require("multiparty");
+var morgan      = require("morgan");
 
-// Configure application
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.set('view engine', 'jade');
-    app.use(express.favicon());
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-    app.use(express.static(join(__dirname, "public")));
+var imageDir    = join( __dirname, "public");
+var app         = module.exports = express();
+
+var ONE_YEAR_MILLISECONDS = 3.154e10;
+var OK                    = 200;
+
+app.set("port", process.env.PORT || 8080);
+app.set('view engine', 'jade');
+app.use(morgan());
+
+function createRandomWriteStream (callback) {
+  var name = Math.random().toString(36).slice(2, 10) + ".png"; // \o/
+  var imagePath = join(imageDir, name);
+
+  fs.exists(imagePath, function (exists) {
+    if (exists) {
+      createRandomWriteStream(callback);
+    } else {
+      callback(fs.createWriteStream(imagePath));
+    }
+  });
+}
+
+app.post("/", function (req, res, next) {
+  var form = new multiparty.Form();
+
+  form.on('part', function (part) {
+    if (part.name !== 'imagedata') return part.resume();
+
+    createRandomWriteStream(function (writeStream) {
+      part.pipe(writeStream).on("close", function () {
+        res.writeHead(OK, {'content-type': 'text/plain'});
+        res.end("http://i.ralph.io/" + writeStream.path.split("/").pop());
+      });
+    });
+  });
+
+  form.on('error', next);
+
+  form.parse(req);
 });
 
-// Handle incoming image posts
-app.post("/", function( req, res, next ) {
-  new formidable.IncomingForm().parse( req, function( err, fields, files ) {
+app.get("/:id.png", function (req, res, next) {
+  var imgPath = join(imageDir, req.params.id + ".png");
 
-    (function write() {
-      var name = Math.random().toString(36).slice(2, 10) + ".png",
-          imagePath = join( imageDir, name );
+  fs.exists(imgPath, function(exists) {
+    var ua = req.headers["user-agent"] || "";
+    var isTwitterBot = ua.toLowerCase().indexOf("twitterbot") > -1 && !req.query.s;
 
-      fs.exists( imagePath, function( exists ) {
-        if ( exists ) {
-          write();
-        } else {
-          fs.rename( files.imagedata.path, imagePath );
-          res.writeHead( 200, {'content-type': 'text/plain'} );
-          res.end("http://i.ralph.io/" + name );
-        }
+    if (exists && isTwitterBot) {
+      fs.stat(imgPath, function(err, stats) {
+        res.render(join(__dirname, "views", "image.jade"), {
+          id: req.params.id,
+          time: moment(stats.ctime).format("LLL")
+        });
       });
-    })();
+    } else {
+      next();
+    }
   });
 });
 
-/**/
-app.get("/:id-card.png", function( req, res ) {
-  fs.exists(join(__dirname, "public", req.params.id + ".png"), function(exists) {
-    if (exists) {
-      fs.createReadStream(join(__dirname, "public", req.params.id + ".png")).pipe(res);
-    } else {
-      res.send(404);
-    }
-  })
-});
+app.use(serveStatic(imageDir, {
+  maxAge: ONE_YEAR_MILLISECONDS
+}));
 
-app.get("/:id.png", function( req, res ) {
-  var imgPath = join(__dirname, "public", req.params.id + ".png");
-  fs.exists(imgPath, function(exists) {
-    if (exists) {
-      if (req.headers["user-agent"] && req.headers["user-agent"].toLowerCase().indexOf("twitterbot") > -1) {
-        fs.stat(imgPath, function(err, stats) {
-          res.render(join(__dirname, "views", "image.jade"), {
-            id: req.params.id,
-            time: moment(stats.ctime).format("LLL")
-          });
-        });
-      } else {
-        fs.stat(imgPath, function (err, stats) {
-          res.setHeader("Content-Type", "image/png");
-          res.writeHead(200, {
-            "Content-Type": "image/png",
-            "Cache-Control": "max-age=31556926",
-            "Expires": new Date(Date.now() + 345600000).toUTCString(),
-            "Content-Length": stats.size
-          });
-          fs.createReadStream(imgPath).pipe(res);
-        });
-      }
-    } else {
-      res.send(404);
-    }
-  })
-});
-
-try {
-  npid.create("/var/run/grabs.pid", true);
-} catch (e) {
-  console.error(e);
+if (!module.parent) {
+  app.listen(app.get("port"), function () {
+    console.info("Grabs server listening on port", app.get("port"));
+  });
 }
-
-module.exports = app;
-
-app.listen( 8080 );
